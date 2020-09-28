85b7913247f0e1d94a9c35958972610244e76499
xgx127 <xgx127@126.com>
2020-09-28 18:37:43 +0800

    Update Practice 11.3 dd2hex.c

---
+++ CS:APP_Practices/Chapter_11/dd2hex.c
@@ -0,0 +1,28 @@
+/*************************************************************************
+	> File Name: dd2hex.c
+	> Author: GeekPanda
+	> Mail: xgx127@126.com 
+	> Created Time: 2020年09月28日 星期一 18时01分04秒
+ ************************************************************************/
+
+#include "../include/csapp.h"
+
+int main(int argc, char **argv)
+{
+	uint32_t addrn;
+	uint32_t addrh;
+	char buf[MAXBUF];
+
+	if (argc != 2){
+		fprintf(stderr, "usage: %s <dot decimal number>", argv[0]);
+		exit(0);
+	}
+	sscanf(argv[1], "%s", buf);
+	if (!inet_pton(AF_INET, buf, &addrn))
+		perror("inet_pton");
+    addrh = ntohl(addrn);	
+	printf("0x%x\n", addrh);
+
+	exit(0);
+}
+
